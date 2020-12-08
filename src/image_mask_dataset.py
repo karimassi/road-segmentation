da3575f6c48a51570f9c41f19a7c5d9b47a45fdc
reljadev <milan54321@hotmail.com>
2020-12-08 10:07:13 +0100

    Deterministic rotation

--- src/image_mask_dataset.py
+++ src/image_mask_dataset.py
@@ -4,23 +4,24 @@ import os
 import matplotlib.image as mpimg
 from PIL import Image
 import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset
 
 # TODO: load to device
 def load_mask(path):
   gt = torch.tensor(mpimg.imread(path))
-  first_channel = (gt > 0.5) * 1.0
-  second_channel = (gt < 0.5) * 1.0
+  road = (gt > 0.5) * 1.0
+  background = (gt < 0.5) * 1.0
   mask = torch.zeros([2, gt.shape[0], gt.shape[1]], dtype=torch.float32)
   
-  mask[0] = first_channel
-  mask[1] = second_channel
+  mask[0] = road
+  mask[1] = background
   
   return mask
 
 class ImageMaskDataset(Dataset):
     
-    def __init__(self, img_dir, gt_dir, transform = None):
+    def __init__(self, img_dir, gt_dir, angle):
       super().__init__()
 
       self.img_dir = img_dir
@@ -29,19 +30,17 @@ class ImageMaskDataset(Dataset):
       #if transform is None:
       #  transform = transforms.Compose([])
 
-      transform = transforms.Compose([
-          transforms.ToTensor()
-      ])
+      pil_to_tensor = transforms.ToTensor()
 
       #load data
       self.files = os.listdir(img_dir)
       self.n = len(self.files)
       
       #self.images = [io.read_image(img_dir + self.files[i]) for i in range(self.n)]
-      self.images = [transform(Image.open(img_dir + self.files[i]).convert('RGB')) for i in range(self.n)]
+      self.images = [TF.rotate(pil_to_tensor(Image.open(img_dir + self.files[i]).convert('RGB')), angle) for i in range(self.n)]
       #self.masks = [torch.tensor(mpimg.imread(gt_dir + self.files[i])).unsqueeze(dim=0) for i in range(self.n)]
       #self.masks = [mpimg.imread(gt_dir + self.files[i]) for i in range(self.n)]
-      self.masks = [load_mask(gt_dir + self.files[i]) for i in range(self.n)]
+      self.masks = [TF.rotate(load_mask(gt_dir + self.files[i]), angle) for i in range(self.n)]
       
 
     def __len__(self): 
